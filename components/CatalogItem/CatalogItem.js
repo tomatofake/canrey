16a9c8c90c26fd0cbc5a64ec2255dd70cb98ecde
tomatofake <dimashulikin@gmail.com>
2025-05-14 15:18:51 +0300

    catalog fixes on mobile deevices

--- components/CatalogItem/CatalogItem.js
+++ components/CatalogItem/CatalogItem.js
@@ -2,13 +2,19 @@ import Image from "next/image";
 
 const CatalogItem = ({ title, src }) => {
   return (
-    <div className="relative bg-white flex h-[500px] hover:scale-[1.01] transform-gpu transition-all">
-      <Image className="object-cover object-center" src={src} alt={title} width={570} height={550} />
+    <div className="relative bg-white flex h-[500px] w-full max-w-[570px] hover:scale-[1.01] transform-gpu transition-all overflow-hidden">
+      <Image
+        src={src}
+        alt={title}
+        width={570}
+        height={550}
+        className="w-full h-full object-cover object-center"
+      />
       <div className="absolute text-white text-center text-3xl z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-nowrap">
         <h2 className="inline-block">{title}</h2>
       </div>
-      <div className="absolute w-full h-full bg-black/65 top-0"></div>
-      <div className="absolute h-full w-full transition-all hover:bg-black/35"></div>
+      <div className="absolute w-full h-full bg-black/65 top-0 left-0 z-10" />
+      <div className="absolute w-full h-full top-0 left-0 transition-all hover:bg-black/35 z-20" />
     </div>
   );
 };
